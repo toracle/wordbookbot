484de4c7e2665ef4f8c686c8049c30bed56c7317
Jeongsoo Park <toracle@gmail.com>
2018-01-27 23:29:51 +0900

    update to use decorator

--- bothub/bot.py
+++ bothub/bot.py
@@ -2,52 +2,31 @@
 
 from bothub_client.bot import BaseBot
 from bothub_client.messages import Message
+from bothub_client.decorators import command
 
 
 class Bot(BaseBot):
-    """Represent a Bot logic which interacts with a user.
-
-    BaseBot superclass have methods belows:
-
-    * Send message
-      * self.send_message(message, user_id=None, channel=None)
-    * Data Storage
-      * self.set_project_data(data)
-      * self.get_project_data()
-      * self.set_user_data(data, user_id=None, channel=None)
-      * self.get_user_data(user_id=None, channel=None)
-
-    When you omit user_id and channel argument, it regarded as a user
-    who triggered a bot.
-    """
-
-    def handle_message(self, event, context):
+    def on_default(self, event, context):
         content = event['content']
-        if content.startswith('/'):
-            if content == '/start':
-                self.send_start_message(event)
-            elif content.startswith('/list'):
-                self.send_word_list(event)
-            return
-
         self.count_word(content)
         self.search_word(event)
 
-    def send_start_message(self, event):
+    @command('start')
+    def send_start_message(self, event, context, args):
         message = Message(event).set_text("Hi! I'm a wordbook bot.\n"\
                                           'Enter a word, you gets dictionary URL.\n'\
                                           'You can check search history afterword.')\
                                 .add_postback_button('Word list', '/list')
         self.send_message(message)
 
-    def send_word_list(self, event):
+    @command('list')
+    def send_word_list(self, event, context, args):
         content = event['content']
-        splitted = content.split()
 
         words_to_count = self.get_user_data()
         sorted_list = sorted(words_to_count.items(), key=lambda d: d[1], reverse=True)
 
-        start_pos = int(splitted[1]) if len(splitted) > 1 else 0
+        start_pos = int(args[1]) if len(args) > 1 else 0
         paged_list = sorted_list[start_pos:start_pos+10]
         word_list = '\n'.join([
             '{}, {}'.format(word, count)
@@ -67,7 +46,7 @@ class Bot(BaseBot):
         data.setdefault(word, 0)
         data[word] += 1
         self.set_user_data(data)
-        
+
     def search_word(self, event):
         content = event['content']
         url = 'http://www.ldoceonline.com/dictionary/{}'.format(content)
